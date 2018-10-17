794ceff415c156ade3bd15a1189c9cf1f6c726ad
MiguelCLG <miguelclg@hotmail.com>
2018-10-17 17:21:23 +0100

    add shooting

---
+++ Shoot'Em Up/Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float speed = 1000f;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update () {
+        rb.velocity = transform.right * speed * Time.deltaTime;
+        if (transform.position.y > 5)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
