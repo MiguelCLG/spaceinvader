98a9419e4c72194a24ea1ef81eb25a591116cd40
MiguelCLG <miguelclg@hotmail.com>
2018-10-17 16:43:04 +0100

    First Commit

---
+++ Shoot'Em Up/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 10f;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update () {
+        Move();
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.A) && transform.position.x > -4f)
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D) && transform.position.x < 4f)
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W) && transform.position.y < 4f)
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S) && transform.position.y > -4f)
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+    }
+}
